bb62a305f797b5e502085e15dae9b7a2d75b3fc9
jack110530 <jack110530@163.com>
2020-12-19 23:01:14 +0800

    Initial commit

---
+++ Example/SFMacro/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  SFMacro
+//
+//  Created by jack110530 on 12/19/2020.
+//  Copyright (c) 2020 jack110530. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
